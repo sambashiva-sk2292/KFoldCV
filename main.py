082a2357aa539ad8c5c4ea59fdc6a7db73fda473
Joseph Guzman <josephfguzman@gmail.com>
2020-02-21 16:40:55 -0700

    added graphs

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ import statistics
 warnings.simplefilter('error') # treat warnings as errors
 
 from matplotlib.pyplot import figure
-figure(num=None, figsize=(15, 8), dpi=80, facecolor='w', edgecolor='k')
+figure(num=None, figsize=(30, 8), dpi=80, facecolor='w', edgecolor='k')
 matplotlib.rc('font', size=24)
+from gradient import Gradient, GradientDescent, MeanLogisticLoss
 
-def KFoldCV(X_mat,y_vec,function,fold_vec, num_neighbors):
+def KFoldCV(X_mat,y_vec,my_function,fold_vec, num_neighbors):
     error_vec = list()
     X_subsets = list()
     y_subsets = list()
@@ -46,10 +47,21 @@ def KFoldCV(X_mat,y_vec,function,fold_vec, num_neighbors):
         y_train=np.concatenate(y_train)
         y_new=y_subsets[i]
         # three times?
-        pred_new=function(X_train,y_train,X_new,num_neighbors)
-        pred_new=function(X_train,y_train,X_new,5,20)
-        pred_new=function(X_train,y_train,X_new)
-
+        pred_new = None
+        if(my_function.__name__ == 'ComputePredictions'):
+            pred_new=my_function(X_train,y_train,X_new,num_neighbors)
+        elif(my_function.__name__ == 'NearestNeighborsCV'):
+            pred_new=my_function(X_train,y_train,X_new)
+            pred_new=pred_new[0]
+        elif(my_function.__name__ == 'Baseline'):
+            pred_new=my_function(X_train,y_train,X_new)
+        elif(my_function.__name__ == 'OneNearestNeighbors'):
+            pred_new=my_function(X_train,y_train,X_new)
+        elif(my_function.__name__ == 'GradientDesent'):
+            weight_mat = my_function(X_train,y_train,0.1,800)
+            pred_new = np.matmul(X_new, weight_mat)
+        else:
+            raise Exception("Unexpected function")
         error_vec.append(100 * (np.mean(y_new[:, 0] != pred_new)))
     error_vec = np.array(error_vec)
     return error_vec
@@ -123,41 +135,6 @@ def Parse(fname):
         std = np.std(temp_ar[:, col])
         if(std == 0):
             print("col " + str(col) + " has an std of 0")
-      atter(test_error_vec, 4 * [str(best_neighbours) + '-NearestNeighbors'])
-plt.xlabel("Mean Validation Error %")
-plt.ylabel("algorithm")
-plt.savefig("test_errors.png")
-plt.clf()
-
-# a table of counts with a row for each fold
-X_subsets = list()
-y_subsets = list()
-num_rows = X_mat.shape[0]
-for i in range(1, 4 + 1):
-    row_nums = list()
-    for j in range(test_fold_vec.shape[0]):
-        if(i == test_fold_vec[j]):
-            row_nums.append(j)
-    X_subsets.append(np.copy(X_mat[row_nums]))
-    y_subsets.append(np.copy(y_vec[row_nums]))
-print('            y')
-print('  {0: >10} {1: >4} {2: >4}'.format('set', '0', '1'))
-for i in range(num_folds):
-    print('  {0: >10} {1: >4} {2: >4}'.format('Fold'+str(i),                                     
-                                          str(((y_subsets[i] == 0).sum()),
-                                          str((y_subsets[i] == 1).sum())))
-
-    
-# IT WILL USE THE ENTIRE DATASET
-# WE SPLIT IT INTO FOUR PARTS AND USE ALL THREE ALGORITHMS
-# YOU SHOULD ACCOUNT FOR EDGE CASE WHERE 1s equal 0s
-# IT'LL BE EASY TO JUST RANDOMLY PICK A WINNER
-
-# 5 20 5 gives us k = 1
-# 5 20 1 gives us k = 1
-# 10 20 2 gives us k = 1
-# 8 20 2 ives us k = 3  temp_ar[:, col] = stats.zscore(temp_ar[:, col])
-    np.random.shuffle(temp_ar) # shuffle rows, set of columns remain the same
     return temp_ar
 
 if len(sys.argv) < 4:
@@ -175,15 +152,11 @@ seed = int(sys.argv[3])
 np.random.seed(seed)
 temp_ar = Parse("spam.data")
 # temp_ar is randomly shuffled at this point
-num_rows = temp_ar.shape[0]
 
 X = temp_ar[:, 0:-1] # m x n
 X = X.astype(float)
 y = np.array([temp_ar[:, -1]]).T # make it a row vector, m x 1
 y = y.astype(int)
-
-# print("error %: " + str(100 * (np.mean(y_new[:, 0] != pred_new))))
-# import pdb; pdb.set_trace()
 
 pred_new,mean_validation_error,min_error,best_neighbours = NearestNeighborsCV(X, y, np.array([]), 5, 20)
 x = [i for i in range(1, len(mean_validation_error) + 1)]
@@ -192,36 +165,91 @@ plt.scatter(best_neighbours, min_error, marker='o', edgecolors='r', s=160, facec
 plt.xlabel("Number of Neighbors")
 plt.ylabel("Mean Validation Error %")
 plt.legend()
+plt.tight_layout()
 plt.savefig("validation_error.png")
 plt.clf()
 
 num_rows = X.shape[0]
 # create random fold vec
-test_fold_vec = np.random.randint(1, 4 + 1, num_rows) 
-test_error_vec = KFoldCV(X, y, ComputePredictions, test_fold_vec, best_neighbours)
-plt.scatter(test_error_vec, 4 * [str(best_neighbours) + '-NearestNeighbors'])
+test_fold_num = 4
+test_fold_vec = np.random.randint(1, test_fold_num + 1, num_rows)
+import pdb; pdb.set_trace()
+test_error_vec_baseline = KFoldCV(X, y, Baseline, test_fold_vec, best_neighbours)
+plt.scatter(test_error_vec_baseline, test_fold_num * ['Baseline'], color='black')
+test_error_vec_nncv = KFoldCV(X, y, NearestNeighborsCV, test_fold_vec, best_neighbours)
+plt.scatter(test_error_vec_nncv, test_fold_num * [str(best_neighbours) + '-NN'], color='red')
+test_error_vec_onn = KFoldCV(X, y, OneNearestNeighbors, test_fold_vec, best_neighbours)
+plt.scatter(test_error_vec_onn, test_fold_num * ['OneNN'], color='blue')
 plt.xlabel("Mean Validation Error %")
 plt.ylabel("algorithm")
+plt.tight_layout()
 plt.savefig("test_errors.png")
+plt.clf()
+
+auc = {'baseline': [], 'k-nn': [], 'one-nn': []}
+for i in range(test_fold_num):
+    test_indices = np.where(test_fold_vec == i + 1)[0]
+    train_indices = np.where(test_fold_vec != i + 1)[0]
+    train_x = X[train_indices]
+    train_y = y[train_indices]
+    test_x = X[test_indices]
+    test_y = y[test_indices]
+    baseline_preds = Baseline(train_x, train_y, test_x)
+    k_nn_preds = ComputePredictions(train_x, train_y, test_x, best_neighbours)
+    one_nn_preds = ComputePredictions(train_x, train_y, test_x, 1)
+    
+    fpr, tpr, baseline_thresholds = metrics.roc_curve(test_y, baseline_preds, pos_label=1)
+    plt.plot(fpr, tpr, linestyle='dotted', label="baseline" if i == 0 else "", color='black', linewidth=3, alpha=0.5)
+    auc['baseline'].append(metrics.roc_auc_score(test_y, baseline_preds))
+    
+    fpr, tpr, k_nn_thresholds = metrics.roc_curve(test_y, k_nn_preds, pos_label=1)
+    plt.plot(fpr, tpr, label="k-nn" if i == 0 else "", color='red', linewidth=3, alpha=0.5)
+    auc['k-nn'].append(metrics.roc_auc_score(test_y, k_nn_preds))
+    
+    fpr, tpr, one_nn_thresholds = metrics.roc_curve(test_y, one_nn_preds, pos_label=1)
+    plt.plot(fpr, tpr, linestyle='dashed', label="one-nn" if i == 0 else "", color='blue', linewidth=3, alpha=0.5)
+    auc['one-nn'].append(metrics.roc_auc_score(test_y, one_nn_preds))
+plt.legend()
+plt.tight_layout()
+plt.savefig("roc_curves.png")
+plt.clf()
+
+plt.xlabel("area")
+plt.ylabel("algorithm")
+for algorithm in auc:
+    for value in auc[algorithm]:
+        my_color = None
+        if(algorithm == 'baseline'):
+            my_color = 'black'
+        elif(algorithm == 'k-nn'):
+            my_color = 'red'
+        elif(algorithm == 'one-nn'):
+            my_color = 'blue'
+        plt.scatter(value, algorithm, color=my_color)
+plt.legend()
+plt.tight_layout()
+plt.savefig("auc_plot.png")
 plt.clf()
 
 # a table of counts with a row for each fold
 X_subsets = list()
 y_subsets = list()
-num_rows = X_mat.shape[0]
-for i in range(1, 4 + 1):
+for i in range(1, test_fold_num + 1):
     row_nums = list()
     for j in range(test_fold_vec.shape[0]):
         if(i == test_fold_vec[j]):
             row_nums.append(j)
-    X_subsets.append(np.copy(X_mat[row_nums]))
-    y_subsets.append(np.copy(y_vec[row_nums]))
+    X_subsets.append(np.copy(X[row_nums]))
+    y_subsets.append(np.copy(y[row_nums]))
+
 print('            y')
 print('  {0: >10} {1: >4} {2: >4}'.format('set', '0', '1'))
-for i in range(num_folds):
-    print('  {0: >10} {1: >4} {2: >4}'.format('Fold'+str(i),                                     
-                                          str(((y_subsets[i] == 0).sum()),
-                                          str((y_subsets[i] == 1).sum())))
+for i in range(test_fold_num):
+    zero_count = (y_subsets[i] == 0).sum()
+    one_count = (y_subsets[i] == 1).sum()
+    print('  {0: >10} {1: >4} {2: >4}'.format('Fold' + str(i),
+                                              str(zero_count),
+                                              str(one_count)))
 
 # IT WILL USE THE ENTIRE DATASET
 # WE SPLIT IT INTO FOUR PARTS AND USE ALL THREE ALGORITHMS
